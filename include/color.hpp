ec1e6db47ccb4c7cdbcaaf16dcf4456c146df959
Artin Alavi <1361714+Arastais@users.noreply.github.com>
2023-10-02 17:04:05 -0600

    Remove macros in implementation of color and style
    Remove X-macros in the header files for color and style, making them much more readable overall.
    This removal has no tangible changes on the code or its generation whatsoever.

--- include/color.hpp
+++ include/color.hpp
@@ -4,46 +4,40 @@
 #include "modifiers.hpp"
 
 
-#define OCT_LWCL_COLORS(black_val) \
-C(black, = black_val)	   \
-C(dark_red,)			   \
-C(dark_green,)			   \
-C(dark_yellow,)			   \
-C(dark_blue,)			   \
-C(dark_magenta,)		   \
-C(dark_cyan,)			   \
-C(light_gray,)			   \
-						   \
-C(dark_gray, = black + 60) \
-C(red,)					   \
-C(green,)				   \
-C(yellow,)				   \
-C(blue,)				   \
-C(magenta,)				   \
-C(cyan,)				   \
-C(white,)				   
-
-
 namespace oct {
 namespace lwcl {
 	namespace color {
 		namespace foreground {
-			enum class values : uint_fast8_t {
-				#define C(name, ...) name __VA_ARGS__,
-				OCT_LWCL_COLORS(30)
-				#undef C
-			};
-			
-			#define C(name, ...) using name = modifiers<static_cast<uint_fast8_t>(values::name)>;
-			OCT_LWCL_COLORS(30)
-			#undef C
+			constexpr std::uint8_t base = 30; //This is the only difference between foreground and background
+			using black        = modifiers<base + 0>;
+			using dark_red     = modifiers<base + 1>;
+			using dark_green   = modifiers<base + 2>;
+			using dark_yellow  = modifiers<base + 3>;
+			using dark_blue    = modifiers<base + 4>;
+			using dark_magenta = modifiers<base + 5>;
+			using dark_cyan    = modifiers<base + 6>;
+			using dark_white   = modifiers<base + 7>;
+
+			constexpr std::uint8_t light_base = base + 60;
+			using light_black  = modifiers<light_base + 0>;
+			using red          = modifiers<light_base + 1>;
+			using green        = modifiers<light_base + 2>;
+			using yellow       = modifiers<light_base + 3>;
+			using blue         = modifiers<light_base + 4>;
+			using magenta      = modifiers<light_base + 5>;
+			using cyan         = modifiers<light_base + 6>;
+			using white        = modifiers<light_base + 7>;
+
+			using light_gray = dark_white;
+			using dark_gray  = light_black;
 
 
 			template<uint8_t byte_value>
-			using indexed = modifiers<38, 5, byte_value>;
+			using indexed = modifiers<base + 8, 5, byte_value>;
+
 
 			template<uint8_t red, uint8_t green, uint8_t blue>
-			using rgb = modifiers<38, 2, red, green, blue>;
+			using rgb = modifiers<base + 8, 2, red, green, blue>;
 
 			template<uint32_t rgb_hex_value>
 			using rgb_hex = rgb<(rgb_hex_value >> 16) & 0xFF, (rgb_hex_value >> 8) & 0xFF, rgb_hex_value & 0xFF>;
@@ -54,26 +48,41 @@ namespace lwcl {
 }
 }
 
+
 namespace oct {
 namespace lwcl {
 	namespace color {
 		namespace background {
-			enum class values : uint_fast8_t {
-				#define C(name, ...) name  __VA_ARGS__,
-				OCT_LWCL_COLORS(40)
-				#undef C
-			};
-			
-			#define C(name, ...) using name = modifiers<static_cast<uint_fast8_t>(values::name)>;
-			OCT_LWCL_COLORS(40)
-			#undef C
+			constexpr std::uint8_t base = 40; //This is the only difference between foreground and background
+			using black        = modifiers<base + 0>;
+			using dark_red     = modifiers<base + 1>;
+			using dark_green   = modifiers<base + 2>;
+			using dark_yellow  = modifiers<base + 3>;
+			using dark_blue    = modifiers<base + 4>;
+			using dark_magenta = modifiers<base + 5>;
+			using dark_cyan    = modifiers<base + 6>;
+			using dark_white   = modifiers<base + 7>;
+
+			constexpr std::uint8_t light_base = base + 60;
+			using light_black  = modifiers<light_base + 0>;
+			using red          = modifiers<light_base + 1>;
+			using green        = modifiers<light_base + 2>;
+			using yellow       = modifiers<light_base + 3>;
+			using blue         = modifiers<light_base + 4>;
+			using magenta      = modifiers<light_base + 5>;
+			using cyan         = modifiers<light_base + 6>;
+			using white        = modifiers<light_base + 7>;
+
+			using light_gray = dark_white;
+			using dark_gray  = light_black;
 			
 
 			template<uint8_t byte_value>
-			using indexed = modifiers<48, 5, byte_value>;
+			using indexed = modifiers<base + 8, 5, byte_value>;
+
 
 			template<uint8_t red, uint8_t green, uint8_t blue>
-			using rgb = modifiers<48, 2, red, green, blue>;
+			using rgb = modifiers<base + 8, 2, red, green, blue>;
 
 			template<uint32_t rgb_hex_value>
 			using rgb_hex = rgb<(rgb_hex_value >> 16) & 0xFF, (rgb_hex_value >> 8) & 0xFF, rgb_hex_value & 0xFF>;
--- include/style.hpp
+++ include/style.hpp
@@ -4,43 +4,32 @@
 #include "modifiers.hpp"
 
 
-#define OCT_LWCL_STYLES \
-S(bold, = 1)			  \
-S(light,)				  \
-S(italic,)				  \
-S(underline,)			  \
-S(strikethrough, = 9)	  \
-						  \
-S(primary_font,)		  \
-S(alt_font_1,)			  \
-S(alt_font_2,)			  \
-S(alt_font_3,)			  \
-S(alt_font_4,)			  \
-S(alt_font_5,)			  \
-S(alt_font_6,)			  \
-S(alt_font_7,)			  \
-S(alt_font_8,)			  \
-S(alt_font_9,)			  \
-						  \
-S(double_underline, = 21) \
-						  \
-S(framed, = 51)			  \
-S(encircled,)			  \
-S(overlined,)
-
-
 namespace oct {
 namespace lwcl {
 	namespace style {
-		enum class values : uint_fast8_t {
-			#define S(name, ...) name __VA_ARGS__,
-			OCT_LWCL_STYLES
-			#undef S
-		};
-		
-		#define S(name, ...) using name = modifiers<static_cast<uint_fast8_t>(values::name)>;
-		OCT_LWCL_STYLES
-		#undef S
+		using bold          = modifiers<1>;
+		using light         = modifiers<2>;
+		using italic        = modifiers<3>;
+		using underline     = modifiers<4>;
+		using strikethrough = modifiers<9>;
+
+		constexpr std::uint8_t font_base = 10;
+		using primary_font = modifiers<font_base + 0>;
+		using alt_font_1   = modifiers<font_base + 1>;
+		using alt_font_2   = modifiers<font_base + 2>;
+		using alt_font_3   = modifiers<font_base + 3>;
+		using alt_font_4   = modifiers<font_base + 4>;
+		using alt_font_5   = modifiers<font_base + 5>;
+		using alt_font_6   = modifiers<font_base + 6>;
+		using alt_font_7   = modifiers<font_base + 7>;
+		using alt_font_8   = modifiers<font_base + 8>;
+		using alt_font_9   = modifiers<font_base + 9>;
+
+		using double_underline = modifiers<21>; 
+
+		using framed    = modifiers<51>;
+		using encircled = modifiers<52>;
+		using overlined = modifiers<53>;
 	}
 }
 }
